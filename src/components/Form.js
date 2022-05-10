2e83d3b1c09e2f2e02207f006969afe354abf035
Luukkasz <l.machalewski@gmail.com>
2022-05-10 16:05:08 +0200

    add RWD

--- src/components/Form.js
+++ src/components/Form.js
@@ -29,14 +29,16 @@ function Form({setInputText, inputText, setTodos, setStatus}) {
 
     return (
         <form onSubmit={handleSubmit}>
-            <input
-                onChange={handleChange}
-                value={inputText}
-                type="text"
-                className="todo-input"/>
-            <button className="todo-button" type="submit">
-                <i className="fas fa-plus-square"></i>
-            </button>
+            <div className="todo-input__wrapper">
+                <input
+                    onChange={handleChange}
+                    value={inputText}
+                    type="text"
+                    className="todo-input"/>
+                <button className="todo-button" type="submit">
+                    <i className="fas fa-plus-square"></i>
+                </button>
+            </div>
             <div className="select">
                 <select onChange={statusHandler} name="todos" className="filter-todo">
                     <option value="all">All</option>
